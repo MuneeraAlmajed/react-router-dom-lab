42aa8bd9ec520c896a11a8f4bb8de23c85b97129
MuneeraAlmajed <muneeraalmajed00@gmail.com>
2026-09-07 20:19:17 +0300

    LAB FINISHED

--- src/components/LetterForm/LetterForm.jsx
+++ src/components/LetterForm/LetterForm.jsx
@@ -2,12 +2,12 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const LetterForm = (props) => {
-    const navigate = useNavigate();
-    const [formData, setFormData] = useState({
-        mailboxId: "",
-        recipient: "",
-        message: "",
-    });
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    mailboxId: props.mailboxes[0]?._id || "",
+    recipient: "",
+    message: "",
+  });
 
   const handleChange = (event) => {
     setFormData({
@@ -18,13 +18,13 @@ const LetterForm = (props) => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-
+    console.log(formData);
     props.addLetter({
-        ...formData,
-        mailboxId: Number(formData.mailboxId),
-    })
-    navigate(`/mailboxes/${formData.mailboxId}`)
-  }
+      ...formData,
+      mailboxId: Number(formData.mailboxId),
+    });
+    navigate(`/mailboxes/${formData.mailboxId}`);
+  };
 
   return (
     <main>
@@ -32,7 +32,13 @@ const LetterForm = (props) => {
 
       <form onSubmit={handleSubmit}>
         <label htmlFor="mailboxId">Mailbox: </label>
-        <select id="mailboxId" name="mailboxId" onChange={handleChange}>
+        <select
+          id="mailboxId"
+          name="mailboxId"
+          value={formData.mailboxId}
+          onChange={handleChange}
+        >
+          {" "}
           {props.mailboxes.map((mailbox) => (
             <option key={mailbox._id} value={mailbox._id}>
               {mailbox._id}
@@ -41,10 +47,15 @@ const LetterForm = (props) => {
         </select>
 
         <label htmlFor="recipient">Recipient: </label>
-        <input type="text" id="recipient" name="recipient" onChange={handleChange} />
+        <input
+          type="text"
+          id="recipient"
+          name="recipient"
+          onChange={handleChange}
+        />
 
         <label htmlFor="message">Message: </label>
-        <textarea id="message" name="message" onChange={handleChange}/>
+        <textarea id="message" name="message" onChange={handleChange} />
 
         <button type="submit">Send Letter</button>
       </form>
